88ba070245e24ea50ba542e89cd397c499312a8c
jjeome <76250325+jjeome@users.noreply.github.com>
2022-05-19 18:51:09 +0900

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -14,40 +14,18 @@ public class Bank {
 		System.out.println(" ---------------------------- ");
 		System.out.println(" 1.예금 | 2.출금 | 3.잔고 | 4.종료 ");
 		System.out.println(" ---------------------------- ");
+                System.out.println("선택> "+menu);
 			if(menu ==1) {
-				System.out.println("선택> "+menu);
+				
 				System.out.println("예금액> "+balance);
 			} else if (menu == 2) {
-				System.out.println("선택> "+menu);
 				System.out.println("출금액> "+balance);
 			} else if (menu == 3) {
-				System.out.println("선택> "+menu);
 				System.out.println("잔고> "+balance);
 			} else {
 				System.out.println("프로그램 종료");
 			}
-                        break;
+                        run = false;
 		}
 		
-		/*int result;
-		do {
-			if (menu == 1) {
-				result = balance;
-			} else if(menu == 2) {
-				result = balance;
-			} else if(menu == 3) {
-				result = balance;
-			} else if(menu == 4) {
-				result = balance;
-			} else {
-				break;
-			}
-			System.out.println(result);
-		} while(true);
-		System.out.println("프로그램 종료");
-		}
-		*/
 		
-	}
-
-}
